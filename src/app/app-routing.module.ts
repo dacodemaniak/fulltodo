274de3fc7e5b73dcc5c92d49298980fdc9c9d616
Jean-Luc <jean-luc.a@ideafactory.fr>
2019-10-08 08:51:22 +0200

    Added User Module and Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,22 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    canActivate: null
+  },
+  {
+    path: 'authentication',
+    loadChildren: './modules/user/user.module#UserModule'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
